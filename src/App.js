13fa48eac7e92e989496dc19582b4556597d23c3
dunba149 <dunba149@d.umn.edu>
2025-12-19 02:09:41 -0600

    Create basic layout (#1)
    * added a profile picture, basic descriptions, and an introduction to the site
    
    * added some more changes to the basic layout
    
    * made a dum little change
    
    * added a work History Section and created the basic layout for it
    
    * added a projects area with the same theming as the work History and changed the color scheme to look a little better
    
    * made some formatting changes and corrected some spacing elements
    
    * added some formatting fixes to the sidebar, need to implement them elsewhere first
    
    * made some format changes
    
    * added a part of the new skills section
    
    * basic layout finished for now, moving onto more in depth changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,173 @@
-import logo from './logo.svg';
 import './App.css';
+import profileImage from "./ProfilePicture.jpg";
 
 
 function Titles() {
   return (
-    <div className="titles"> 
+    <div className="titles">
       <p>
-        current layout
+        About Me
       </p>
       <p>
         change up right here
       </p>
-      <div>
-        <p>
-          change even furthur
-        </p>
+      <p>
+        Work History
+      </p>
+      <p>
+        My Resume
+      </p>
 
-      </div>
-        
+
+
     </div>
   );
 }
 
+function Introduction() {
+  return (
+    <div className="MyName">
+
+      Trey Dunbar
+    </div>
+  )
+}
+
+function MyDescription() {
+  return (
+
+    <div className="myPicture">
+
+      <div className="myDescription">
+
+        <img src={profileImage} alt="profile" className="profile" />
+
+        <div className="TheDescription">
+          <p>
+            I'm a Computer Science major at the University of Minnesota Duluth, pursuing a Math minor to deepen my analytical and quantitative skills. Alongside my CS coursework, I've completed Calculus I & II, Differential Equations, Linear Algebra, Statistics, and Intro to Statistical Computing, which sharpened my ability to reason through complex systems, analyze data, and apply mathematical models to real-world problems.
+          </p>
+          <p>
+            On the CS side, I've already completed Software Engineering and Automata & Formal Languages, Software Analysis & Design, Computer Architecture, and Discrete Structures. These experiences have given me a strong foundation in both the theory of computation and the practical skills of building reliable software systems. Upcoming coursework in Security and Operating Systems will further expand my expertise.
+          </p>
+          <p>
+            I've applied this knowledge in hands-on projects such as Game Board Nexus, a mobile app connecting board game enthusiasts, and a Tabletop Game Tracker web app with Firebase integration. I'm proficient in C++, Java, Python, and Dart/Flutter, with growing experience in HTML, CSS, and JavaScript, and I'm comfortable using Git/GitHub for version control. Tutoring computer science for two semesters has also strengthened my ability to explain complex concepts clearly, reinforcing both my technical depth and communication skills.
+
+          </p>
+          <p>
+            I'm seeking internship opportunities where I can contribute to real-world projects, learn from experienced developers, and grow as a software engineer. I bring curiosity, adaptability, and a strong work ethic to every challenge, with the added perspective of a solid math and statistics background that enhances my problem-solving and data-driven thinking.
+          </p>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function WorkHistory() {
+  return (
+
+    <div className='Columns'>
+
+      <div className='Section'>
+        <p className='Header'>
+          hello
+          <p className='Description'>
+            hello
+          </p>
+        </p>
+      </div>
+      <div className='Section'>
+        <p className='Header'>
+          hello
+        </p>
+      </div>
+      <div className='Section'>
+        <p className='Header'>
+          hello
+        </p>
+      </div>
+    </div>
+  );
+}
+
+
+
+function Projects() {
+  return (
+    <div className='Columns'>
+      <div className='Section'>
+
+
+        <p>
+          Project1
+        </p>
+      </div>
+      <div className='Section'>
+        <p>
+          Project2
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Skills() {
+  return (
+    <div className='Columns' style = {{backgroundColor: 'goldenrod', borderRadius: '8px', boxShadow: '1vw .5vw 1vw black', marginRight: '1.5vw', padding: '0px'}}>
+      <div className='Box'>
+        <ul>
+          <li>first skill</li>
+          <li>second SKill</li>
+        </ul>
+      </div>
+      <div className='Box'>
+        <p>
+          first skill
+        </p>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <Titles />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          this is my resume
-        </p>
 
-      </header>
+    <div className="app-layout">
+      <Titles />
+
+      <div className="App">
+        <Introduction />
+        <p className="AboutMe">
+          About Me:
+        </p>
+        <MyDescription />
+        <p className='MainTitleOption'>
+          Work History:
+        </p>
+        <WorkHistory />
+        <p className='MainTitleOption'>
+          Projects:
+        </p>
+        <Projects />
+        <p className = "MainTitleOption">
+          Skills: 
+        </p>
+        <Skills />
+        <div className='borderOfButton'>
+          <button className="ResumeButton"
+            onClick={() => window.open("https://drive.google.com/file/d/1PGdTQ20wvCBvQlYjCU4523KnN6_rI9wk/view?usp=sharing", "_blank")
+              
+
+            }
+          >
+            See My Resume!
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
